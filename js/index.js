1a78f3e13d9b56b7bd9f552e2e2470754fbbd8b6
rosen777 <rosen.toshev@flatironschool.com>
2018-12-12 10:30:36 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
-}
+
+let saveLincoln = document.getElementById('save_lincoln')
+
+saveLincoln.addEventListener('click', function(event) {
+  let timeout = document.getElementById('interval').value
+  let foregroundImage = document.getElementById('foreground')
+    setTimeout(function() {foregroundImage.style.display = 'none';}, timeout)
+});
