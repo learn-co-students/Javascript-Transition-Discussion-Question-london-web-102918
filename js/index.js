22736b1e7f0dbf525fce9a64b016d269d8c925a9
Paul-Cl-ark <clark.paul123@gmail.com>
2018-12-12 10:34:20 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
+document.querySelector('#save_lincoln').addEventListener('click', function (event) {
+  saveLincoln(parseInt(document.querySelector('#interval').value))
+})
+
+function saveLincoln (interval) {
+  if (isNaN(interval)) {
     interval = 10000
   }
-  $('#foreground').fadeOut(interval);
+  let fg = document.querySelector('#foreground')
+  let fadeEffect = setInterval(function () {
+    if (!fg.style.opacity) {
+      fg.style.opacity = 1
+    }
+    if (fg.style.opacity < 0.1) {
+      clearInterval(fadeEffect)
+    } else {
+      fg.style.opacity -= 0.1
+    }
+  }, interval)
 }
